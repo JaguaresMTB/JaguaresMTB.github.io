3f7c79900f46037494b88311ec3e020c7732a411
Jaguares MTB Yuctan <jaguaresyuc@gmail.com>
2025-05-02 12:34:26 -0600

    Lista de cumple actualizada

--- script/birthday.js
+++ script/birthday.js
@@ -9,23 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             window.birthdayData = data;
-
-            const uniqueMap = new Map();
-            birthdayData.forEach(item => {
-              const key = `${item.name}|${item.date}`;
-              if (!uniqueMap.has(key)) {
-                uniqueMap.set(key, item);
-              }
-            });
-            const uniqueData = Array.from(uniqueMap.values());
-        
-            // Ordenar por fecha
-            uniqueData.sort((a, b) => new Date(a.date) - new Date(b.date));
-            birthdayData=uniqueData;
+            birthdayData = SortByName(birthdayData);
             var now = new Date();
             var currentMonth = now.getMonth() + 1; // 0-indexed
 
-            updateBirthday(currentMonth);
+            const urlParams = new URLSearchParams(window.location.search);
+            const showAll = urlParams.get('showall')=="true";
+
+            updateBirthday(currentMonth,showAll);
         })
         .catch(error => {
             console.error('Error al cargar el JSON:', error);
@@ -33,7 +24,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-function updateBirthday(monthToShow){
+function SortByName(dataToSort) {
+    const uniqueMap = new Map();
+    dataToSort.forEach(item => {
+      const key = `${item.name}|${item.date}`;
+      if (!uniqueMap.has(key)) {
+        uniqueMap.set(key, item);
+      }
+    });
+  
+    const uniqueData = Array.from(uniqueMap.values());
+    uniqueData.sort((a, b) => a.name.localeCompare(b.name, 'es', { sensitivity: 'base' }));  
+    return uniqueData;
+  }
+
+function SortByDate(dataToSort){
+    const uniqueMap = new Map();
+    dataToSort.forEach(item => {
+      const key = `${item.name}|${item.date}`;
+      if (!uniqueMap.has(key)) {
+        uniqueMap.set(key, item);
+      }
+    });
+    const uniqueData = Array.from(uniqueMap.values());
+    uniqueData.sort((a, b) => new Date(a.date) - new Date(b.date));
+    return uniqueData;
+}
+
+function updateBirthday(monthToShow, isShowByName){
     const tbody = document.querySelector('#birthday-table tbody');
     const formatter = new Intl.DateTimeFormat('es-MX', {
       day: 'numeric',
@@ -45,13 +63,18 @@ function updateBirthday(monthToShow){
     cboMes[monthToShow].selected=true;
     tbody.replaceChildren();
 
+    if(isShowByName)
+    {
+        birthdayData = SortByName(birthdayData);
+    }
+
     birthdayData.forEach(entry => {
         const [year, month, day] = entry.date.split('-');
         var newdate = new Date(year, month - 1, day);
-        // var newdate = new Date(entry.date);
+        
         var elementdate = newdate.getMonth() + 1;
 
-        if (elementdate === monthToShow) {    
+        if ((!isShowByName && elementdate === monthToShow) || isShowByName) {    
 
             const tr = document.createElement('tr');
 
